54707529011d57fc40f0f90162f35e640ae3f451
yanglulu <yanglu_u@126.com>
2021-03-20 22:47:54 +0800

    逆波兰表达式求值

---
+++ src/main/java/utils/Assert.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Assert {
+
+    public static void assertTrue(boolean assertion) {
+
+        if (!assertion) {
+            throw new RuntimeException("assertion error.");
+        }
+    }
+
+    public static void assertEquals(int assertion, int expect) {
+
+        if (assertion != expect) {
+            throw new RuntimeException("assertion error. expect: " + assertion + ", actual: " + expect);
+        }
+    }
+
+}
