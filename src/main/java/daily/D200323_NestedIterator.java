b4a2f5bb6062ca81b2ed71cfbc8bb635544ad43e
yanglulu <yanglu_u@126.com>
2021-03-23 20:17:41 +0800

    每日一题

---
+++ src/main/java/daily/D200323_NestedIterator.java
@@ -0,0 +1,74 @@
+package daily;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ * <p>
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,4,6]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/flatten-nested-list-iterator
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
+class NestedIterator implements Iterator<Integer> {
+
+    private List<Integer> list;
+
+    private Iterator<Integer> iterator;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        list = new ArrayList<>();
+        traverse(nestedList);
+        iterator = list.iterator();
+    }
+
+    private void traverse(List<NestedInteger> nestedList) {
+
+        for (NestedInteger nestedInteger : nestedList) {
+
+            if (nestedInteger.isInteger()) {
+
+                list.add(nestedInteger.getInteger());
+            } else {
+
+                traverse(nestedInteger.getList());
+            }
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return iterator.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+
+}
